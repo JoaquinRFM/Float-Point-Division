839fea091734889839b735d58a0f69ea0c6e26c5
Ronald Joaquin Flores Matheos <131552885+JoaquinRFM@users.noreply.github.com>
2025-04-14 20:54:12 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <algorithm>
 using namespace std;
 
 void getData(float& n1, float& n2) {
@@ -101,6 +100,7 @@ int verify(vector<bool> binaryFloat) {
         }
         return 3; //Valor Infinito
     }
+    return 0;
 }
 
 void printBinaryFloat(vector<bool> binaryFloat, float number) {
@@ -119,274 +119,52 @@ void printBinaryFloat(vector<bool> binaryFloat, float number) {
             break;
         case 2:
             cout << "Valor NaN" <<endl;
-        break;
+            break;
         case 3:
             if (binaryFloat[0]) {
                 cout << "-";
             } else {
                 cout << "+";
             }
-        cout << "Infinito" << endl;
-        break;
-    }
-}
-
-int binaryToInteger(vector<bool> binaryFloat) {
-    int integer = 0;
-    for (int i = 8; i > 0; i--) {
-        if (binaryFloat[i] == 1) {
-            integer += pow(2, 8 - i);
-        }
-    }
-    return integer;
-}
-
-vector<bool> significandConversion(vector<bool> binaryFloat, int exponent) {
-    vector<bool> significandImplicit;
-    // Bit implícito: 1 para normales, 0 para subnormales/cero
-    bool implicitBit = (exponent != 0);
-    significandImplicit.push_back(implicitBit);
-
-    // Añadir los 23 bits de la mantisa (bits 9-31)
-    for (int i = 9; i < 32; i++) {
-        significandImplicit.push_back(binaryFloat[i]);
-    }
-
-    return significandImplicit;
-}
-
-void normalizeSubnormal(int& exponent, vector<bool>& significand) {
-    if (exponent != 0) return; // Solo normalizar si es subnormal
-
-    int shift = 0;
-    // Encontrar el primer 1 en la mantisa
-    for (size_t i = 1; i < significand.size(); i++) {
-        if (significand[i]) {
-            shift = i - 1;
+            cout << "Infinito" << endl;
             break;
-        }
     }
-
-    // Desplazar la mantisa
-    for (size_t i = 1; i < significand.size() - shift; i++) {
-        significand[i] = significand[i + shift];
-    }
-    for (size_t i = significand.size() - shift; i < significand.size(); i++) {
-        significand[i] = false;
-    }
-
-    // Ajustar exponente
-    exponent = 1 - shift;
 }
 
-bool isGreaterOrEqual(const vector<bool>& a, const vector<bool>& b) {
-    if (a.size() != b.size()) return a.size() > b.size();
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] && !b[i]) return true;
-        if (!a[i] && b[i]) return false;
+int exponentToInt(vector<bool> binaryNumber){
+    int number = 0;
+    for (int i = 8; i > 0; i--){
+        number += pow(2, 8 - i);
     }
-    return true;
+    return number;
 }
 
-vector<bool> subtractMantissas(vector<bool> a, const vector<bool>& b) {
-    bool borrow = false;
-    for (int i = a.size()-1; i >= 0; --i) {
-        bool b_val = i < b.size() ? b[i] : false;
-        if (borrow) {
-            if (a[i]) {
-                a[i] = false;
-                borrow = false;
-            } else {
-                a[i] = true;
-            }
-        }
-        if (!a[i] && b_val) {
-            borrow = true;
-            a[i] = true;
-        } else {
-            a[i] = a[i] ^ b_val;
-        }
+vector<bool> significandImplicit(vector<bool> binaryNumber){
+    vector<bool> significand;
+    significand.push_back(1);
+    for (int i = 9; i < 32; i++){
+        significand.push_back(binaryNumber[i]);
     }
-    return a;
+    return significand;
 }
 
-
-vector<bool> divideSignificands(vector<bool> mantisa1, vector<bool> mantisa2) {
-    vector<bool> resultado;
-    vector<bool> acumulador(mantisa1.size(), false);
-
-    // Asegurarnos que ambas mantisas tengan el mismo tamaño
-    if (mantisa1.size() < mantisa2.size()) {
-        mantisa1.insert(mantisa1.begin(), mantisa2.size() - mantisa1.size(), false);
-    } else if (mantisa2.size() < mantisa1.size()) {
-        mantisa2.insert(mantisa2.begin(), mantisa1.size() - mantisa2.size(), false);
-    }
-
-    // Agregar bits extra para precisión
-    int bitsExtra = mantisa1.size();
-    mantisa1.insert(mantisa1.end(), bitsExtra, false);
-
-    // División bit a bit
-    for (size_t i = 0; i < mantisa1.size(); ++i) {
-        // Desplazar el acumulador a la izquierda
-        acumulador.erase(acumulador.begin());
-        acumulador.push_back(mantisa1[i]);
-
-        // Comparar acumulador con divisor (mantisa2)
-        if (isGreaterOrEqual(acumulador, mantisa2)) {
-            // Restar mantisa2 del acumulador
-            acumulador = subtractMantissas(acumulador, mantisa2);
-            resultado.push_back(true);
-        } else {
-            resultado.push_back(false);
-        }
-    }
-
-    // Normalizar el resultado (eliminar ceros a la izquierda)
-    auto first_one = find(resultado.begin(), resultado.end(), true);
-    if (first_one != resultado.end()) {
-        resultado.erase(resultado.begin(), first_one);
-    } else {
-        return vector<bool>(mantisa1.size(), false); // Resultado cero
-    }
-
-    // Asegurar que el resultado tenga el tamaño correcto
-    if (resultado.size() > mantisa1.size()) {
-        resultado.resize(mantisa1.size());
-    } else if (resultado.size() < mantisa1.size()) {
-        resultado.insert(resultado.begin(), mantisa1.size() - resultado.size(), false);
-    }
-
-    return resultado;
+vector<bool> significandDivision(vector<bool> significand1, vector<bool> significand2){
+    vector<bool> significandResult;
+    return significandResult;
 }
 
-vector<bool> divideMantissas(vector<bool> dividend, vector<bool> divisor) {
-    vector<bool> result;
-    vector<bool> remainder(dividend.size(), false);
-
-    // Añadir bits extra para precisión
-    dividend.insert(dividend.end(), dividend.size(), false);
-
-    for (size_t i = 0; i < dividend.size(); ++i) {
-        remainder.erase(remainder.begin());
-        remainder.push_back(dividend[i]);
-
-        if (isGreaterOrEqual(remainder, divisor)) {
-            remainder = subtractMantissas(remainder, divisor);
-            result.push_back(true);
-        } else {
-            result.push_back(false);
-        }
-    }
-
-    // Normalizar resultado
-    auto first_one = find(result.begin(), result.end(), true);
-    if (first_one != result.end()) {
-        result.erase(result.begin(), first_one);
-    } else {
-        return vector<bool>(24, false);
-    }
-
-    if (result.size() > 24) {
-        result.resize(24);
-    } else if (result.size() < 24) {
-        result.insert(result.begin(), 24 - result.size(), false);
-    }
-
-    return result;
-}
-
-vector<bool> divisionFloatPoint(float number1, vector<bool> binaryNumber1, float number2, vector<bool> binaryNumber2) {
-    // Extraer componentes
+vector<bool> divisionFloatPoint(float number1, vector<bool> binaryNumber1, float number2, vector<bool> binaryNumber2){
     bool sign1 = binaryNumber1[0];
-    int exponent1 = 0;
-    for (int i = 0; i < 8; i++) {
-        if (binaryNumber1[1+i]) exponent1 += (1 << (7-i));
-    }
-    vector<bool> mantissa1(binaryNumber1.begin()+9, binaryNumber1.end());
-
     bool sign2 = binaryNumber2[0];
-    int exponent2 = 0;
-    for (int i = 0; i < 8; i++) {
-        if (binaryNumber2[1+i]) exponent2 += (1 << (7-i));
-    }
-    vector<bool> mantissa2(binaryNumber2.begin()+9, binaryNumber2.end());
-
-    // Casos especiales
-    if (number2 == 0) {
-        if (number1 == 0) return vector<bool>{0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; // NaN
-        return vector<bool>{sign1^sign2,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; // ±Inf
-    }
-
-    // Añadir bit implícito
-    if (exponent1 != 0) mantissa1.insert(mantissa1.begin(), true);
-    else mantissa1.insert(mantissa1.begin(), false);
-
-    if (exponent2 != 0) mantissa2.insert(mantissa2.begin(), true);
-    else mantissa2.insert(mantissa2.begin(), false);
-
-    // Normalizar subnormales
-    if (exponent1 == 0 && mantissa1[0] == false) {
-        int shift = 0;
-        for (size_t i = 1; i < mantissa1.size(); i++) {
-            if (mantissa1[i]) {
-                shift = i-1;
-                break;
-            }
-        }
-        rotate(mantissa1.begin(), mantissa1.begin()+shift+1, mantissa1.end());
-        fill(mantissa1.end()-shift-1, mantissa1.end(), false);
-        exponent1 = 1 - shift;
-    }
-
-    if (exponent2 == 0 && mantissa2[0] == false) {
-        int shift = 0;
-        for (size_t i = 1; i < mantissa2.size(); i++) {
-            if (mantissa2[i]) {
-                shift = i-1;
-                break;
-            }
-        }
-        rotate(mantissa2.begin(), mantissa2.begin()+shift+1, mantissa2.end());
-        fill(mantissa2.end()-shift-1, mantissa2.end(), false);
-        exponent2 = 1 - shift;
-    }
-
-    // Dividir mantisas
-    vector<bool> resultMantissa = divideMantissas(mantissa1, mantissa2);
-
-    // Calcular exponente resultante
-    int resultExponent = exponent1 - exponent2 + 127;
-
-    // Normalizar resultado
-    if (!resultMantissa.empty() && resultMantissa[0] == false) {
-        int shift = 0;
-        for (size_t i = 1; i < resultMantissa.size(); i++) {
-            if (resultMantissa[i]) {
-                shift = i-1;
-                break;
-            }
-        }
-        rotate(resultMantissa.begin(), resultMantissa.begin()+shift+1, resultMantissa.end());
-        fill(resultMantissa.end()-shift-1, resultMantissa.end(), false);
-        resultExponent -= shift;
-    }
-
-    // Construir resultado
-    vector<bool> result(32, false);
-    result[0] = sign1 ^ sign2;
-
-    for (int i = 0; i < 8; i++) {
-        result[1+i] = (resultExponent >> (7-i)) & 1;
-    }
-
-    for (int i = 0; i < 23; i++) {
-        if (i+1 < resultMantissa.size()) {
-            result[9+i] = resultMantissa[i+1];
-        }
-    }
-
-    return result;
+    bool finalSign = sign1 ^ sign2;
+    int exponent1 = exponentToInt(binaryNumber1);
+    int exponent2 = exponentToInt(binaryNumber2);
+    vector<bool> finalExponent = exponendToBinary(exponent1 - exponent2 + 127);
+    vector<bool> significand1 = significandImplicit(binaryNumber1);
+    vector<bool> significand2 = significandImplicit(binaryNumber2);
+    vector<bool> finalSignificand = significandDivision(significand1, significand2);
+    vector<bool> resultBinary = mergeBinary(finalSign, finalExponent, finalSignificand);
+    return resultBinary;
 }
 
 int main() {
@@ -397,6 +175,6 @@ int main() {
     vector<bool> binaryNumber2 = floatToBinary(number2);
     printBinaryFloat(binaryNumber2, number2);
     vector<bool> binaryResult = divisionFloatPoint(number1, binaryNumber1, number2, binaryNumber2);
-    printBinaryFloat(binaryResult, 0);
+    //printBinaryFloat(binaryResult, 0);
 
 }
